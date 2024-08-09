8313d0fc02f4710719c087fd4d75fc379122da10
ChamithuJ <138036733+ChamithuJ@users.noreply.github.com>
2024-08-09 13:46:52 +0530

    Connected MongoDB database

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,14 @@
 import express from 'express';
-
+import connectDB from './utils/db.js';
+import Log from './models/log.js'; 
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Server is running');
+connectDB().then(() => {
+  
+  app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
+}).catch(error => {
+  console.error('Failed to connect to the database:', error);
 });
+
